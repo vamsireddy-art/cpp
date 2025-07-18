09cc86dcc24cbe484d4fa6aec18f9b9670db5158
vamsireddy-art <mohanvamsireddykarri@gmail.com>
2025-07-18 10:03:29 +0530

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,15 @@
+#include"stack.h"
+using namespace std;
+int main(){
+    int st[n];
+    int top = -1;
+    push(st,top,10);
+    push(st,top,20);
+    push(st,top,30);
+    push(st,top,40);
+    push(st,top,50);
+    pop(st,top);
+    push(st,top,60);
+    push(st,top,70);
+    display(st,top);
+}
